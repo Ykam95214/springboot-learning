cce4c88e9b39af11da68747022a488ab2af9490b
Ykam95214 <ky9521427@gmail.com>
2026-06-05 08:01:04 +0530

    feature: RestController annotation does not require ResponseBody

--- src/main/java/com/sb/rest/rest/practice/controller/JobPostController.java
+++ src/main/java/com/sb/rest/rest/practice/controller/JobPostController.java
@@ -3,20 +3,19 @@ package com.sb.rest.rest.practice.controller;
 import com.sb.rest.rest.practice.model.JobPost;
 import com.sb.rest.rest.practice.service.JobPostService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@Controller
+@RestController
 public class JobPostController {
 
     @Autowired
     JobPostService jobPostService;
 
     @GetMapping("/jobPosts")
-    @ResponseBody
     public List<JobPost> getJobPosts() {
         return jobPostService.getJobPosts();
     }
