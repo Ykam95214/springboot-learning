399cb8ce38c3a4d306dd96f2fa3581b3c8aa61bd
Ykam95214 <ky9521427@gmail.com>
2026-06-05 07:53:34 +0530

    feature: Controller annotation with Response body

--- src/main/java/com/sb/rest/rest/practice/controller/JobPostController.java
+++ src/main/java/com/sb/rest/rest/practice/controller/JobPostController.java
@@ -3,18 +3,20 @@ package com.sb.rest.rest.practice.controller;
 import com.sb.rest.rest.practice.model.JobPost;
 import com.sb.rest.rest.practice.service.JobPostService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 
-@RestController
+@Controller
 public class JobPostController {
 
     @Autowired
     JobPostService jobPostService;
 
-    @GetMapping("/Jobs")
+    @GetMapping("/jobPosts")
+    @ResponseBody
     public List<JobPost> getJobPosts() {
         return jobPostService.getJobPosts();
     }
